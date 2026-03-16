c13c4c9affddc7b0404dba9fe8ba785f1300024f
ana010109 <achausmann@furb.br>
2026-03-16 20:56:46 -0300

    feat: adiciona classe Acao com padrão Observer e gerenciamento de ordens

--- AnaliseAlgoritmos/Problema_2/Acao.cs
+++ AnaliseAlgoritmos/Problema_2/Acao.cs
@@ -1,25 +1,45 @@
 ﻿
+using AnaliseAlgoritmos.Problema_2;
 using Problema_2.Core.Interfaces;
+using System.Runtime.Intrinsics.Arm;
 
 public class Acao : IPublicador
 {
-    private readonly List<IAssinante> _assinantes;
+    private readonly List<IAssinante> _assinantes = new();
+    private readonly List<Ordem> _ordens = new();
 
     public string Nome { get; }
     public decimal ValorAtual { get; private set; }
-  
+
     public Acao(string nome, decimal valorInicial)
     {
         Nome = nome;
         ValorAtual = valorInicial;
     }
-    
-    
+
+    public void AdicionarOrdem(Ordem ordem)
+    {
+        if (ordem is null)
+            throw new ArgumentNullException(nameof(ordem));
+
+        _ordens.Add(ordem);
+    }
+
+    public void RemoverOrdem(Ordem ordem) => _ordens.Remove(ordem);
+
+    public IReadOnlyList<Ordem> ObterOrdens() => _ordens.AsReadOnly();
+
+    public void RegistrarMatch(decimal novoValor)
+    {
+        ValorAtual = novoValor;
+        Notificar();
+    }
+
     public void Inscrever(IAssinante assinante)
     {
         if (assinante is null)
             throw new ArgumentNullException(nameof(assinante));
-            
+
         if (!_assinantes.Contains(assinante))
             _assinantes.Add(assinante);
     }
@@ -28,7 +48,7 @@ public class Acao : IPublicador
     {
         if (assinante is null)
             throw new ArgumentNullException(nameof(assinante));
-        
+
         _assinantes.Remove(assinante);
     }
 
