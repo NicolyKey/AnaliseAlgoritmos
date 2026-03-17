6531fc6c5f6c43db04c50fb91a85ac6eeb337738
ana010109 <achausmann@furb.br>
2026-03-16 21:27:58 -0300

    feat: classe ordem com nome do investidor, tipo e valor

---
+++ AnaliseAlgoritmos/Problema_2/Ordem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnaliseAlgoritmos.Problema_2
+{
+    public class Ordem
+    {
+        public string NomeInvestidor { get; }
+        public TipoOrdem Tipo { get; }
+        public decimal Valor { get; }
+
+        public Ordem(string nomeInvestidor, TipoOrdem tipo, decimal valor)
+        {
+            if (string.IsNullOrWhiteSpace(nomeInvestidor))
+                throw new ArgumentException("Nome do investidor é obrigatório", nameof(nomeInvestidor));
+
+            if (valor <= 0)
+                throw new ArgumentException("O valor da ordem deve ser positivo", nameof(valor));
+
+            NomeInvestidor = nomeInvestidor;
+            Tipo = tipo;
+            Valor = valor;
+        }
+    }
+}
